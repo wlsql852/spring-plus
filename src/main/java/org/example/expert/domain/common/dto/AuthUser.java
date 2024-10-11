55010988ad676c59d42c190244f33ad9ef4b6e5a
wlsql852 <wlsql852@naver.com>
2024-10-11 10:29:12 +0900

    level 2-9 완료

--- src/main/java/org/example/expert/domain/common/dto/AuthUser.java
+++ src/main/java/org/example/expert/domain/common/dto/AuthUser.java
@@ -2,6 +2,11 @@ package org.example.expert.domain.common.dto;
 
 import lombok.Getter;
 import org.example.expert.domain.user.enums.UserRole;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+import java.util.Collection;
+import java.util.List;
 
 @Getter
 public class AuthUser {
@@ -10,11 +15,13 @@ public class AuthUser {
     private final String email;
     private final UserRole userRole;
     private final String nickname;
+    private final Collection<? extends GrantedAuthority> authorities;
 
     public AuthUser(Long id, String email, UserRole userRole, String nickname) {
         this.id = id;
         this.email = email;
         this.userRole = userRole;
         this.nickname = nickname;
+        this.authorities = List.of(new SimpleGrantedAuthority(userRole.name()));
     }
 }
